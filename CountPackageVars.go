b1d6980092101f3744bfe524a43fd8ead031923f
mohira <shukatsudaison@gmail.com>
2022-01-25 21:25:23 +0900

    パッケージ変数の数を数えた。

--- CountPackageVars.go
+++ CountPackageVars.go
@@ -1,6 +1,9 @@
 package CountPackageVars
 
 import (
+	"fmt"
+	"go/ast"
+	"go/token"
 	"golang.org/x/tools/go/analysis"
 )
 
@@ -13,6 +16,53 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	var count int
+	for _, file := range pass.Files {
+		// ASTの確認: ASTをみればパッケージ変数がどう扱われているか検討がつく
+		// ast.Print(pass.Fset, file)
 
+		// ファイル名
+		// fmt.Println(file.Name) // a
+
+		for _, decl := range file.Decls {
+			// 各Declのチェック(VARもFUNCもある)
+			// fmt.Printf("%T %v\n", decl, decl)
+
+			genDecl, ok := decl.(*ast.GenDecl)
+			if !ok {
+				continue // 早期Returnな感じでフィルタリングするほうが読みやすい
+			}
+
+			// token.Tokenをチェック
+			if genDecl.Tok != token.VAR {
+				continue
+			}
+
+			for _, spec := range genDecl.Specs {
+				// 詳細を表すノードに変換(ast.Specと実装している型)
+				s, ok := spec.(*ast.ValueSpec)
+				if !ok {
+					continue
+				}
+				count += len(s.Names)
+				fmt.Printf("%v\n", s.Names)
+				// [name]
+				// [age]
+				// [address]
+				// [_]
+				// [_]
+				// [x y]
+				// [a b c]
+				for _, name := range s.Names {
+					// _ は パッケージ変数とはみなさないものとしておく
+					if name.Name != "_" {
+						count++
+					}
+				}
+			}
+		}
+
+		fmt.Printf("パッケージ変数の数は: %d\n", count)
+	}
 	return nil, nil
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,26 @@
 package a
 
+var (
+	name string
+	age  int
+)
+
+var address string
+var _ = 100
+var _ = 200
+var x, y = 1, 2
+var a, b, c = tuple3()
+
+const Version = "1.0.0"
+
+type (
+	MyInt    int
+	MyString string
+)
+
+func tuple3() (int, int, int) {
+	return 3, 4, 5
+}
+
 func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
 }
